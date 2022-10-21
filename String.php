946413eb7827ebb18eb56e36d9a33c1a26fb38f6
RegaAG <rega22052002@gmail.com>
2022-10-21 19:06:35 +0700

    membuat String

---
+++ String.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "Nama : ";
+echo "Rega Anton Giapierro";
+
+echo <<<REGA
+
+Ini adalah contoh string yang sangat
+
+panjang, dan juga gak perlu ngetik ENTER secara
+manual, "bisa quote" juga
+
+REGA;
